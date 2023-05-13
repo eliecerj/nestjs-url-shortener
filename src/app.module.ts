9686161d3c17c8aa4286fd2b46d012d95c67c03f
Eliecer Arteaga <eliecerj@gmail.com>
2023-05-13 01:58:18 -0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UrlModule } from './url/url.module';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost:12345/urlshortener'), UrlModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
